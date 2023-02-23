b3313e07f8d204cd6413077b643b9b7512b7fa81
HelloSleep <nargene@gmail.com>
2023-02-23 14:58:52 +0000

    not yet

--- Greedy/조이스틱_lvl2.py
+++ Greedy/조이스틱_lvl2.py
@@ -1,6 +1,18 @@
 def solution(name):
     answer = 0
-    arrow = [+1,-1]
+    print(ord('A'), ord('Z'))
+    
+    
 
     for n in name:
+        c = ord(n)
+        if c <77:
+            answer += c-65
+            print(c,chr(c),answer)
+        else:
+            answer += 90-c+1
+            print(c,chr(c),answer)
+        #print(c,answer)
+        answer+=1
+    answer-=1
     return answer
