1db4fd7031d33c08f4a675970d9f974978fcf2a6
HelloSleep <nargene@gmail.com>
2023-03-24 11:33:00 +0000

    fwf

--- Greedy/큰수만들기_lvl1.py
+++ Greedy/큰수만들기_lvl1.py
@@ -7,14 +7,13 @@ def solution(number, k):
     stack = []
 
     for n in number:
-
-        #가장 최근에 push한 숫자보다 낮은 경우 pop 
+        # 가장 최근에 push한 숫자보다 낮은 경우 pop
         while stack and stack[-1] < n and k > 0:
             stack.pop()
             k -= 1
         stack.append(n)
+    # asb
 
-    
     # 아직 제거되지 못 한 숫자를 뒤에서 삭제
     # 예를 들어 91995,3 인경우 for문을 빠져나온뒤 9995 k=2 이기에 앞의 99까지만 출력
     if k > 0:
