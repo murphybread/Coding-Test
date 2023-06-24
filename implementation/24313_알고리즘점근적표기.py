eec27444a80f0c3c8100ff2eaf8f8126d7026c56
HelloSleep <nargene@gmail.com>
2023-06-24 15:18:45 +0000

    Silver1: 10m test all n case

--- implementation/24313_알고리즘점근적표기.py
+++ implementation/24313_알고리즘점근적표기.py
@@ -1,4 +1,12 @@
-a_one, a_zero = map(int,input())
+a_one, a_zero = map(int,input().split())
 c =int(input())
-
 n_zero =int(input())
+check =0
+
+for i in range(n_zero ,101):
+    if a_one * i +a_zero > c * i:
+        print(0)
+        check = 1
+        break
+if check == 0 :
+    print(1)
