7a3638481344adbc5b3cb48f46e51f6180bea176
murphybread <nargene@gmail.com>
2023-08-07 21:19:48 +0900

    1254좋다.py: gold4 two-pointer 1h

--- Data-Structure/Two-Pointer/1254좋다.py
+++ Data-Structure/Two-Pointer/1254좋다.py
@@ -1,41 +1,44 @@
 # Ai 값 -10억~+10억
 # 문제해석 N은 2000까지, 수의 위치가 다르면 다른수 2000 2000은 다른 수
-# set을 사용
-
 
 # 좋은 수를 찾는다
-# - (오름차순)
-# - 두 개의 합임으로 num보다 작은 두 수를 투포인터로
-# - 가장 작은 수와 바로 다음 수의 합이 num보다 크다면 무시
-# - 가장 작은 수와 바로 다음 수의 합이 num보다 작다면 start 움직이기
-# Counter로 set 형태의 중복제거 자료형을 만든다
-# Counter로 좋은수가 몇개인지 센다
-import sys
+# - (오름차순 정렬)
+# - start를 0, end를 N-1로 원소의 왼쪽 끝에서 오른쪽 끝을 가리키는 투 포인터 선언
+#   - 여기서 헤맸던 부분은 end를 가리키는 i-1로 해야하는 것 아닌가? 였는데  숫자의범위가 -가 포함돼서 오른쪽 끝이 맞음
+#      - test case -4 0 4, start가 0번째 원소, end가 N-1일 경우에야 count가 됨
+# - i가 start나 end일 때는 pass
+# N =2000이기에 O(N) 가능. sort함수 O(NlogN)
 
-input = sys.stdin.readline
+
 N = int(input())
 array = list(map(int, input().split()))
 
 array.sort()
 count = 0
 
-
-
-for i in range(2,len(array)):
+for i in range(N):
     start = 0
-    end = 1
-    internal_sum = array[start]
-    while internal_sum < array[i] and end < i:
-        internal_sum += array[end]
-        end +=1
-        if end == i-1:
-            internal_sum -= start
+    end = N - 1
+    while start < end:
+        if start == i:
             start += 1
-            end = start +1
-    if internal_sum == num:
-        count +=1
-print(count)        
+            continue
+        if end == i:
+            end -= 1
+            continue
         
+        internal_sum = array[start] + array[end]
+        
+        if internal_sum == array[i]:
+            count += 1
+            break
+        elif internal_sum < array[i]:
+            start += 1
+        else:
+            end -= 1
+
+print(count)
+
     
     
 
