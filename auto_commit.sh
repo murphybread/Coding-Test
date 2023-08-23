ba43120aba020d3ca5274df54beffa6b0b431767
HelloSleep <nargene@gmail.com>
2023-08-23 21:39:21 +0900

    10989수정렬하기3.py auto_commit.sh git: bronze1 Algorithm-Sort 10m

---
+++ auto_commit.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# Check the number of parameters
+if [ $# -ne 2 ]; then
+  echo "Usage: $0 <level> <taken time>"
+  echo "Example: $0 silver1 15m"
+  exit 1
+fi
+
+# Get the level and time parameters
+level="$1"
+time="$2"
+
+# Stage all changes in the current directory and its subdirectories
+git add .
+if [ $? -ne 0 ]; then
+  echo "Error staging files. Make sure you're in a git repository."
+  exit 1
+fi
+
+# Get the list of changed files
+changed_files=$(git diff --name-only --cached)
+if [ $? -ne 0 ]; then
+  echo "Error getting changed files. Make sure you have staged changes."
+  exit 1
+fi
+
+# Extract the category from the path (replace slashes with dashes)
+category=$(dirname "$changed_files" | tr '/' '-')
+
+# Extract only the filename from the path
+filename=$(basename "$changed_files")
+
+# Create a commit message with the filename, level, category, and time
+commit_message="$filename: $level $category $time"
+
+# Commit with the constructed message
+git commit -m "$commit_message"
