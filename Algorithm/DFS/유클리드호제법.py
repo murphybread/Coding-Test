8ad1a3d4d5f148886832430269c628b524ba53cc
HelloSleep <nargene@gmail.com>
2023-09-01 22:07:35 +0900

    유클리드호제법.py: bronze4 Algorithm-DFS 5m

---
+++ Algorithm/DFS/유클리드호제법.py
@@ -0,0 +1,9 @@
+# A,B가 주어 졌을 때(A>B) A를 B로 나눈 후 나머지를 R이라고 하자
+# 이 때 A와 B의 최대공약수는 B와 R의 최대공약수와 같다.
+# 즉 A -> B , B->R
+
+def gcd(A,B):
+    if A % B == 0:
+        return B
+    else:
+        return gcd(B , A%B)
