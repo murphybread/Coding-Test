5755dc46b09dde03e4bd104f2a89b48cba22126f
HelloSleep <nargene@gmail.com>
2023-09-09 21:26:22 +0900

    미로탈출.py: lvl2,42% Algorithm-BFS 55m52s,chatgpt

--- Algorithm/BFS/미로탈출.py
+++ Algorithm/BFS/미로탈출.py
@@ -1,47 +1,57 @@
-"""
-NxM크기의 미로
-출발위치는 (1,1) 출구는 (N,M) 괴물은 0, 통로는 1
-최소탈출하기 위한 칸의 개수, 시작칸과 마지막칸 고려
-"""
+# https://school.programmers.co.kr/learn/courses/30/lessons/159993
 
+# bfs
 
-# 최소칸 즉 BFS문제
-# 2차원 미로 받기
-# bfs에서 예외 칸을벗어남, 0일경우 제외
-# 상하좌우를 위한 nx,ny설정
 from collections import deque
 
-def bfs(x,y):
-
-    queue =deque()
-    queue.append((x,y))
-
-    while queue:
-        x,y = queue.popleft()
-
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-
-            if nx<0 or nx>=n or ny<0 or ny>=m:
-                continue
-
-            if graph[nx][ny] == 0:
-                continue
-
-            if graph[nx][ny] == 1:
-                graph[nx][ny] = graph[x][y] +1
-                queue.append((nx,ny))
+def solution(maps):
+    # 4방향을 확인하기위한 튜플 리스트
+    directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
 
 
-    return graph[n-1][m-1]
+    # 시작위치를 튜플쌍(x,y)으로 받고 목적지도 동일한 구조로 받음. visited의경우 S->L과 L->E를 따로 구분함
+    def bfs(start, end, visited):
+        # bfs를 활용하기 위해 queue에 넣음
+        queue = deque([(start, 0)])
+        while queue:
+            (x, y), dist = queue.popleft()
+            # 도착할 경우 return 이동한 시간
+            if (x, y) == end:
+                return dist
+            # 상화좌우를 확인
+            for dx, dy in directions:
+                nx, ny = x + dx, y + dy
+                # 먼저 맵을 벗어나는지 확인
+                if 0 <= nx < len(maps) and 0 <= ny < len(maps[0]):
 
-n,m = map(int,input().split())
+                    # 맵을 안 벗어난 상태에서 방문했는지 and 벽인지 확인
+                    if not visited[nx][ny] and maps[nx][ny] != 'X':
+                        # 방문 처리
+                        visited[nx][ny] = True
+                        # 다음 위치를 queue에 넣은 후 길이를 증가
+                        queue.append(((nx, ny), dist + 1))
+        return -1
 
-graph = []
-for _ in range(n):
-    graph.append(list(map(int,input())))
-dx=[-1,1,0,0]
-dy=[0,0,-1,1]
+    start, lever, exit = None, None, None
+    for i in range(len(maps)):
+        for j in range(len(maps[0])):
+            if maps[i][j] == 'S':
+                start = (i, j)
+            elif maps[i][j] == 'L':
+                lever = (i, j)
+            elif maps[i][j] == 'E':
+                exit = (i, j)
 
-print(bfs(0,0))
+    # S-L당시의 visited로 구현
+    visited1 = [[False] * len(maps[0]) for _ in range(len(maps))]
+    dist_lever = bfs(start, lever, visited1)
+    if dist_lever == -1:
+        return -1
+
+    # L-E당시의 visited로 구현
+    visited2 = [[False] * len(maps[0]) for _ in range(len(maps))]
+    dist_exit = bfs(lever, exit, visited2)
+    if dist_exit == -1:
+        return -1
+
+    return dist_lever + dist_exit
