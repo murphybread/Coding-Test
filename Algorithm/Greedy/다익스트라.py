835813f4ca48c920b56f1fe6e6d6ee2800d7adfd
HelloSleep <nargene@gmail.com>
2023-09-18 23:02:26 +0900

    다익스트라.py: ? Algorithm-Greedy 35m

---
+++ Algorithm/Greedy/다익스트라.py
@@ -0,0 +1,63 @@
+import heapq
+
+n, m = map(int, input().split())
+INF = int(1e9)
+start = int(input())
+
+graph = [[] for _ in range(n + 1)]
+visited = [False] * (n + 1)
+distance = [INF] * (n + 1)
+
+for _ in range(m):
+    a, b, c = map(int, input().split())
+    graph[a].append((b, c))
+
+
+# def min_idx():
+#     min_value = INF
+#     idx = 0
+
+#     for i in range(1, n + 1):
+#         if distance[i] < min_value and not visited[i]:
+#             min_value = distance[i]
+#             idx = i
+#     return idx
+
+
+def dijkstra(start):
+    q = []
+    heapq.heappush(q, (0, start))
+    distance[start] = 0
+
+    while q:
+        dist, now = heapq.heappop(q)
+
+        if distance[now] < dist:
+            continue
+
+        for i in graph[now]:
+            cost = dist + i[1]
+            if cost < distance[i[0]]:
+                distance[i[0]] = cost
+                heapq.heappush(q, (cost, i[0]))
+
+    visited[start] = True
+    for j in graph[start]:
+        distance[j[0]] = j[1]
+
+    for i in range(n - 1):
+        now = min_idx()
+        visited[now] = True
+
+        for j in graph[now]:
+            cost = distance[now] + j[1]
+            if cost < distance[j[0]]:
+                distance[j[0]] = cost
+
+
+dijkstra(start)
+
+if distance[start] == INF:
+    print(-1)
+else:
+    print(distance[i])
