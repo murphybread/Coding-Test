74e2262f4ebf34ddc1ae41c819b473ebe9a3a747
HelloSleep <nargene@gmail.com>
2023-09-20 00:04:06 +0900

    feat: round

--- Implementation/solvedac.py
+++ Implementation/solvedac.py
@@ -3,20 +3,23 @@ import sys
 input = sys.stdin.readline
 n = int(input())
 
+
+def my_round(x):
+    if x - int(x) >= 0.5:
+        return int(x) + 1
+    else:
+        return int(x)
+
+
 if n == 0:
     print(0)
 else:
     problem = []
     for _ in range(n):
         problem.append(int(input()))
-    print(problem)
 
-    cut = round(len(problem) * 0.15)
+    cut = my_round(len(problem) * 0.15)
     problem.sort()
 
     ac = problem[cut : len(problem) - cut]
-
-    if len(ac) == 0:
-        print(0)
-    else:
-        print(round(sum(ac) / len(ac)))
+    print(my_round(sum(ac) / len(ac)))
