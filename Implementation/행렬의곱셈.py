ef0b2b990ea51bb739b5bce43ab8a85242bd2fe6
Murphybread <nargene@gmail.com>
2023-10-03 15:55:51 +0900

    행렬의곱셈.py: update Implementation ver1

--- Implementation/행렬의곱셈.py
+++ Implementation/행렬의곱셈.py
@@ -12,7 +12,5 @@ def solution(arr1, arr2):
         for j in range(len(arr1[0])):
             for k in range(len(arr2[0])):
                 answer[i][k] += arr1[i][j] * arr2[j][k]
-                # print(i,j,k,arr1[i][j] *arr2[j][k])
-                # answer[i].append(arr1[i][j] *arr2[j][k])
 
     return answer
