9443839af976b1a9649b169d17a15c12cf63a06a
Murphybread <nargene@gmail.com>
2024-01-05 22:43:35 +0900

    카펫.py: lvl2 Algorithm-Brute_force-카펫.py Implementation 10m

--- Algorithm/Brute_force/카펫.py
+++ Algorithm/Brute_force/카펫.py
@@ -1,22 +1,31 @@
-# https://school.programmers.co.kr/learn/courses/30/lessons/42842
+"""
+문제 설명
+Leo는 카펫을 사러 갔다가 아래 그림과 같이 중앙에는 노란색으로 칠해져 있고 테두리 1줄은 갈색으로 칠해져 있는 격자 모양 카펫을 봤습니다.
 
-# 수식 세우기
-# 특정 조건을 만족하는 x,y찾기
+carpet.png
 
-# 넓이 = x *y , x>=y ,5000 and 2,000,000
-# 노랑 = (x-2) * (y-2)
-# 갈색은 2x+2y = 2(x+y) -4
-# 식  xy = xy -2(x+y) +4 + 2(x+y) -4
-# xy = yellow + brown
-# x = (yellow+brown)/y
+Leo는 집으로 돌아와서 아까 본 카펫의 노란색과 갈색으로 색칠된 격자의 개수는 기억했지만, 전체 카펫의 크기는 기억하지 못했습니다.
 
-# 마지막 검증: xy가여러 쌍이 나올 수 있는데 이 때 brown = 2(x+y) -4를 검증
+Leo가 본 카펫에서 갈색 격자의 수 brown, 노란색 격자의 수 yellow가 매개변수로 주어질 때 카펫의 가로, 세로 크기를 순서대로 배열에 담아 return 하도록 solution 함수를 작성해주세요.
+
+제한사항
+갈색 격자의 수 brown은 8 이상 5,000 이하인 자연수입니다.
+노란색 격자의 수 yellow는 1 이상 2,000,000 이하인 자연수입니다.
+카펫의 가로 길이는 세로 길이와 같거나, 세로 길이보다 깁니다.
+입출력 예
+"""
+
 
 def solution(brown, yellow):
     answer = []
 
-    for i in range(1, brown):
-        y = (brown + yellow) // i
-        if i >= y and (2 * (i + y) - 4) == brown:
-            return [i, y]
+    # x*y = brown + yellow
+    # y
+    # brown = 2x+2y -4 =
+    x, y = 0, 0
+    for i in range(1, brown + 1):
+        if (2 * i + 2 * (brown + yellow) / i) - 4 == brown:
+            x = i
+            y = int((brown + yellow) / x)
 
+    return [max(x, y), min(x, y)]
